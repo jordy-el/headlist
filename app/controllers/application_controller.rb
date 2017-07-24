513c188fad906ac6a19b7c42efb5a7f73ccca92c
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-07-24 19:54:26 +1000

    Create 'addable?' helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :addable?
+
+  def addable?
+    @user != current_user &&
+      !@user.friends_with?(current_user) &&
+        !@user.pending_friends.include?(current_user) &&
+          !current_user.pending_friends.include?(@user)
+  end
+
   protected
 
   def configure_permitted_parameters
