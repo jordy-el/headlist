94288759e9dd72bd3a4dc7c7aa5e7b5e6b0f649e
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-07-31 21:01:29 +1000

    Remove addition of facebook avatar on sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,12 +33,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.name[0...auth.info.name.chars.index(' ')]
       user.last_name = auth.info.name[(auth.info.name.chars.index(' ') + 1)..-1]
-      user.avatar = auth.info.image
-      # user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      #user.avatar = auth.info.image
     end
   end
 
